c46f4902e43135f51943ad91390eed6ef9fb7ff2
mandeepmourya007 <mandeepmourya007@gmail.com>
2018-06-29 19:37:38 +0530

    registration login logout done

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from .forms import student
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
 def log_in(request):
-    return render(request,"accounts/login.html")
+
+        if request.method=="POST":
+            print(request.POST)
+            u=request.POST.get("username")
+            p=request.POST.get("password")
+            user=authenticate(request,username=u,password=p)
+            if user is not None:
+                login(request,user)
+                messages.success(request, 'you are login')
+                return HttpResponseRedirect(reverse('home'))
+        return render(request,'accounts/login.html')
+
 
 def sign_up(request):
     if request.method=="POST":
         form=student(request.POST)
+
         if form.is_valid():
             username=form.cleaned_data["username"]
-            pasword=form.cleaned_data["Password1"]
+            password=form.cleaned_data["Password1"]
+            email=form.cleaned_data["email"]
 
-            user=User.objects.create_user(username=username,password=pasword)
+            user=User.objects.create_user(username=username,password=password,email=email)
             messages.success(request, 'account created')
-            return HttpResponseRedirect(reverse("account:login"))
+            print(username,password)
+            return HttpResponseRedirect(reverse('accounts:login'))
     else:
         form=student()
 
